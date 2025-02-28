f2dc7e3d2f9ccdf24fb54b5477dfd26249403b51
Nasara Friday <nasarafriday247@gmail.com>
2025-02-28 16:14:17 +0100

    UPDATE

---
+++ script.js
@@ -0,0 +1,25 @@
+let searchform = document.getElementById('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+  searchform.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+  navbar.classList.toggle('active');
+    searchform.classList.remove('active');
+}
+
+window.onscroll = () => {
+  searchform.classList.remove('active');
+    navbar.classList.remove('active');
+
+    if (window.scrollY > 30) {
+        document.querySelector('header').classList.add('header-active');
+        }
+    else {
+        document.querySelector('header').classList.remove('header-active');
+    }
+}
